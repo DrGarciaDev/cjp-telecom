e847ed5ddc437bfd4fcb3326f8c8f7e9e1132eae
Luis García <luis_grdz@hotmail.com>
2020-05-20 17:53:42 -0500

    Cambio en la migracion users para añadir campo sin añadir una nueva migracion, y usar el comando refresh

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('show_id')->unique()->default(time() + rand(0,50));
             $table->rememberToken();
             $table->timestamps();
         });
